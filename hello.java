d722782119c75cdb2f61102079811b7c802c80d8
Puttu-123 <126179015+Puttu-123@users.noreply.github.com>
2025-01-17 10:28:46 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("Hello, Jenkins!");
-        Sysytem.out.println("hey updated line of the program!");
+        System.out.println("hey updated line of the program!");
     }
 }
