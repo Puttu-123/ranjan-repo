ee23241412985aaee875728644c9c542598b05a7
Puttu-123 <126179015+Puttu-123@users.noreply.github.com>
2025-01-30 13:09:52 +0530

    jan_29th_2nd_Update hello.java

--- hello.java
+++ hello.java
@@ -4,5 +4,6 @@ public class hello {
         System.out.println("hey updated line of the program!");
         System.out.println("second update of hell.java file ");
         System.out.println("todays data is 30/01/2025 ");
+        System.out.println("I scheduled in cronjob let see the result");
     }
 }
